d495b169b7b0827a3ed16beb10939a55de1e87f5
Akram.Home <a.bagha@falnic.com>
2025-01-02 15:41:55 +0330

    ss

--- PHP/Config_DB.php
+++ PHP/Config_DB.php
@@ -6,21 +6,7 @@
 
    
 
-    if( ! defined('merchen')) {
-        define('merchen' , '5c9268b6-f7dd-11e8-bf9e-005056a205be');
-    }
-
-    if( ! defined('url')) {        
-        //define('url' , 'http://sookazan.ir/AddStringForOnlineThem/');
-        define('url' , 'localhost//sookazan/');
-    }
-
-    if( ! defined('step')) {
-        define('step' , '20');
-    }
-
-  
-   $dbLink = mysqli_connect(DB_SERVER, DB_USERNAME , DB_PASSWORD , DB_DATABASE);
+$dbLink = mysqli_connect(DB_SERVER, DB_USERNAME , DB_PASSWORD , DB_DATABASE);
  
 if (mysqli_connect_error()) 
 {
